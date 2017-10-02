827da83860bc7569076f4d4d47a674f1b0300bca
Raphael <raphaelrpais@gmail.com>
2017-10-02 16:35:14 -0300

    mimo-slick+bgresize

---
+++ mimo/mimo-scripts.js
@@ -0,0 +1,9 @@
+//Chama no Slick pra fazer os slide!
+$(document).ready(function(){
+    $('.mimo_home-slides').slick({
+        adaptativeHeight: true,
+        autoplay: false,
+        arrows: false,
+        dots: true
+    });
+});
